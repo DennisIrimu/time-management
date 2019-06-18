2d26090b47127b5ebc95005dda9bec1349fc5794
DennisIrimu <lyricaldnyt@gmail.com>
2019-06-19 01:46:10 +0300

    Created model test class

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.views import status
+from .models import Projects
+from .serializers import SongsSerializer
 
 # Create your tests here.
+
+class BaseViewTest(APITestCase):
+    client = APIClient()
+
+    @staticmethod
+    def create_project(name="", description="", start_date="", duration="",):
+        if name != "" and description != "" and start_date !="" and duration !="":
+            Projects.objects.create(name=name, description=description, start_date=start_date, duration=duration)
+
+    def setUp(self):
+        self.create_project("e-commerce","website to sell vehicles", "Monday", duration="two months")
+
+    
