112b3fdbd3df306f4a7690f3899e20dfc3afb7c7
DennisIrimu <lyricaldnyt@gmail.com>
2019-06-19 01:57:29 +0300

    Created to-do projects model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Projects(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    description = models.CharField(max_length=255, null=False)
+    start_date = models.DateTimeField(auto_now_add=True)
+    duration = models.DateTimeField(auto_now=False)
+
+    def __str__(self):
+        return (self.name)
