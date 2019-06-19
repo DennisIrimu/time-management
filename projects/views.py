c274d1484ab97b1c170dff7d8506cfe78e36f106
DennisIrimu <lyricaldnyt@gmail.com>
2019-06-19 13:52:59 +0300

    Create API view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Projects
+from .rest_framework import generics
+from .serializers import ProjectsSerializer
 # Create your views here.
+
+class ListProjectsView(generics.LisyAPIView):
+    queryset = Projects.objects.all()
+    serializer_class =  ProjectsSerializer
