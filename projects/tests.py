1f1763e9d0235a7e1bb32fad1156138ab0f29ab7
DennisIrimu <lyricaldnyt@gmail.com>
2019-06-19 21:29:47 +0300

    Create user login test

--- projects/tests.py
+++ projects/tests.py
@@ -15,7 +15,30 @@ class BaseViewTest(APITestCase):
         if name != "" and description != "" and start_date !="" and duration !="":
             Projects.objects.create(name=name, description=description, start_date=start_date, duration=duration)
 
+    def login_a_user(self, username="", password="")
+        url = reverse(
+            "auth-login",
+            kwargs={
+                "version":"v1"
+            }
+        )
+        return self.client.post(
+            url,
+            data=json.dumps({
+                "username": username
+                "password": password
+            }),
+            content_type="application/json"
+        )
+
     def setUp(self):
+        self.user = User.objects.create_superuser(
+            username="dnyt",
+            email = "haia@gmail.com",
+            password = "1234567890",
+            first_name = "boro",
+            last_name = "yeng",
+        )
         self.create_project("e-commerce","website to sell vehicles", "Monday", duration="two months")
 
 class GetAllProjectsTest(BaseViewTest):
