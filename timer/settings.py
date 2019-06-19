e3327f545cc7deb13e04dfca934b8204a3be3b5c
DennisIrimu <lyricaldnyt@gmail.com>
2019-06-19 14:29:00 +0300

    Create REST_FRAMEWORK versioning

--- timer/settings.py
+++ timer/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
