b7b065bb6fd0ffbe4ce27f8e43538fb2692d5786
DennisIrimu <lyricaldnyt@gmail.com>
2019-06-20 22:41:28 +0300

    Created and populated base template

--- timer/settings.py
+++ timer/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'timer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -163,3 +163,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/'
