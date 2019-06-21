449285170fa24ce58108358b5a37dd7fedb0aab8
DennisIrimu <lyricaldnyt@gmail.com>
2019-06-21 23:50:02 +0300

    Created manager permissions

---
+++ projects/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import BasePermission
+from .models import Projects
+
+class Manager(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        is isinstance(obj, Projects):
+            return obj.owner = request.user
+        return obj.owner == request.user
